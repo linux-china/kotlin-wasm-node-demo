1b58c44b495feb9708298f6282fe571af493b96b
Libing Chen <libing.chen@gmail.com>
2023-01-01 13:18:55 +0800

    app: dropped

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew build
-cd build/js/packages/nodejs-hello-world-wasm/kotlin; ~/.jsvu/v8-9.2.212 --experimental-wasm-typed-funcref --experimental-wasm-gc --experimental-wasm-eh --module ./nodejs-hello-world-wasm.js

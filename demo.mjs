1b90d9ba0c8256d8885a3ddd8681b3162729e942
Libing Chen <libing.chen@gmail.com>
2023-07-08 22:12:11 +0800

    demo: demo.mjs to test exported methods from Wasm

---
+++ demo.mjs
@@ -0,0 +1,3 @@
+import exports from "./build/js/packages/kotlin-wasm-node-example-wasm/kotlin/kotlin-wasm-node-example-wasm.mjs"
+let welcome = exports.welcome("linux_china");
+console.log(welcome);

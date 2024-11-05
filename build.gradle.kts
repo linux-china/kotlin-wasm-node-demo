2e904d4dd7d4c20be42babc8ac6ee7bbe16f196f
Libing Chen <libing.chen@gmail.com>
2024-11-05 10:56:24 +0800

    build: update to koltin 2.1.0-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 
 plugins {
-    kotlin("multiplatform") version "2.1.0-Beta2"
+    kotlin("multiplatform") version "2.1.0-RC"
 }
 
 group = "com.example"
